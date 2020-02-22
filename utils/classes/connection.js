6a57b892fb3c3b5bad0a769d2fe5572ae3ad244f
celciusjj <juanjjo99@hotmail.es>
2020-02-22 16:21:48 -0500

    Necesito que haga un retrieveteam para cuando se desconecte o se salga de la sala

--- utils/classes/connection.js
+++ utils/classes/connection.js
@@ -1,53 +1,55 @@
 const { verifyCode, addTeam, retrieveCurrentTeams } = require("../sockets");
 
 module.exports = class Connection {
-  constructor() {
-    this.socket;
-  }
+    constructor() {
+        this.socket;
+    }
 
-  async initEvents() {
-    let connection = this;
-    let socket = connection.socket;
+    async initEvents() {
+        let connection = this;
+        let socket = connection.socket;
 
-    socket.emit("main", { message: "Principal" });
+        socket.emit("main", { message: "Principal" });
 
-    socket.on("joinRoom", async data => {
-      let socketData = { code: data.split("|")[0], team: data.split("|")[1] };
-      let res = await verifyCode(socketData.code);
+        socket.on("joinRoom", async data => {
+            let socketData = { code: data.split("|")[0], team: data.split("|")[1] };
+            let res = await verifyCode(socketData.code);
 
-      if (res) {
-        let currentTeams = await retrieveCurrentTeams(data.split("|")[0]);
-        let team = {};
-        if (currentTeams.teams.length < 6) {
-          team = await addTeam(socketData.code, socketData.team);
-          if (team.status) {
-            team.message = "Equipo añadido";
-          } else {
-            team.message = "Error intenta de nuevo";
-          }
-          socket.emit("response", team);
-        } else {
-          team.message = "Ya hay mas de seis equipos en la sala";
-          team.team = [];
-          socket.emit("response", team);
-        }
-      } else {
-        socket.emit("response", {
-          message: "El código no es válido",
-          stutus: false,
-          team: []
+            if (res) {
+                let currentTeams = await retrieveCurrentTeams(data.split("|")[0]);
+                let team = {};
+                if (currentTeams.teams.length < 6) {
+                    team = await addTeam(socketData.code, socketData.team);
+                    if (team.status) {
+                        team.message = "Equipo añadido";
+                    } else {
+                        team.message = "Error intenta de nuevo";
+                    }
+                    socket.emit("response", team);
+                } else {
+                    team.message = "Ya hay mas de seis equipos en la sala";
+                    team.team = [];
+                    socket.emit("response", team);
+                }
+            } else {
+                socket.emit("response", {
+                    message: "El código no es válido",
+                    stutus: false,
+                    team: []
+                });
+            }
         });
-      }
-    });
 
-    socket.on("callTeams", async data => {
-      retrieveCurrentTeams(data).then(result =>
-        socket.emit("getTeams", result.teams)
-      );
-    });
+        socket.on("callTeams", async data => {
 
-    socket.on("disconnect", () => {
-      console.log("desconectado");
-    });
-  }
+            retrieveCurrentTeams(data).then(result => {
+                socket.emit("getTeams", { Items: result.teams })
+                socket.broadcast.emit("getTeams", {Items: result.teams})
+            })
+        });
+
+        socket.on("disconnect", () => {
+            console.log("desconectado");
+        });
+    }
 };
