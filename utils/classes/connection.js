633854f9d78ed410d579051c546244d40f3a686f
DanielBerrioB <daniberrio10@hotmail.com>
2020-02-28 08:40:21 -0500

    New back changes

--- utils/classes/connection.js
+++ utils/classes/connection.js
@@ -28,6 +28,7 @@ module.exports = class Connection {
 
       if (res) {
         let currentTeams = await retrieveCurrentTeams(data.split("|")[0]);
+        currentTeams = currentTeams.length > 0 ? currentTeams : [];
         let team = {};
         let isRepeated = currentTeams.find(e => e.team === socketData.team);
         if (isRepeated) {
