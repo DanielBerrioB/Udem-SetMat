e497896407d2a663c3b6b252b250959315e7a02b
DanielBerrioB <daniberrio10@hotmail.com>
2020-03-23 17:05:33 -0500

    Nuevos cambios

--- __test__/helpers.test.js
+++ __test__/helpers.test.js
@@ -1,8 +1,12 @@
-const { isThereAnyConnection } = require("../utils/helpers");
+const helpers = require("../utils/helpers");
 const client = require("../utils/testMocks");
 
 describe("helpers functions", () => {
   it("isThereAnyConnection", () => {
-    expect(isThereAnyConnection(client)).toBe(false);
+    expect(helpers.isThereAnyConnection(client)).toBe(false);
+  });
+
+  it("expects a random string with a defined length", () => {
+    expect(helpers.generateRandomCode(7)).toHaveLength(7);
   });
 });
